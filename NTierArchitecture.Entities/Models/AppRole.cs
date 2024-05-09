d3354de64a037cfa4850504749ceb8216bbc9d2d
Kadir Gökçe <gokcekadr@gmail.com>
2024-05-09 19:24:55 +0300

    Add project files.

---
+++ NTierArchitecture.Entities/Models/AppRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NTierArchitecture.Entities.Models
+{
+    public sealed class AppRole : IdentityRole
+    {
+    }
+}
---
+++ NTierArchitecture.Entities/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace NTierArchitecture.Entities.Models
+{
+    public sealed class AppUser :IdentityUser<Guid>
+    {
+        public string Name { get; set; }
+        public int LastName { get; set; }
+    }
+}
