28f688c5d6a7a76532e99dbc55cf01318f30c0ff
Kadir Gökçe <gokcekadr@gmail.com>
2024-05-14 18:27:34 +0300

    test commit

--- NTierArchitecture.Entities/Models/AppUser.cs
+++ NTierArchitecture.Entities/Models/AppUser.cs
@@ -3,9 +3,12 @@ using Microsoft.AspNetCore.Identity;
 
 namespace NTierArchitecture.Entities.Models
 {
-    public sealed class AppUser :IdentityUser<Guid>
+    public sealed class AppUser : IdentityUser<Guid>
     {
         public string Name { get; set; }
         public int LastName { get; set; }
     }
+
+
+
 }
