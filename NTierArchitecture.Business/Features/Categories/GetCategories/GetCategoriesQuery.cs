5484e226788e45c66171e5fb3d59cc6fd3d29773
Kadir Gökçe <gokcekadr@gmail.com>
2024-05-28 20:14:27 +0300

    MediatR kütüphanesi eklendi query ve commandlar oluşturuldu(CQRS)

---
+++ NTierArchitecture.Business/Features/Categories/GetCategories/GetCategoriesQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using NTierArchitecture.Entities.Models;
+
+namespace NTierArchitecture.Business.Features.Categories.GetCategories
+{
+    public sealed record GetCategoriesQuery() : IRequest<List<Category>>
+    {
+
+
+    };
+}
